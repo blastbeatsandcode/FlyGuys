60a4b6f555e2618198462ea98d6e0b515508b66f
zeivhann <alexsilcott@gmail.com>
2018-05-05 15:24:43 -0400

    added CollisionsHandler script and updated rocket script; updated rocket physics

---
+++ Assets/Scripts/CollisionsHandler.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CollisionsHandler : MonoBehaviour {
+    // Rigidbody rigidbody;
+
+    // object we want to collide with.
+    [Header("Collision Settings")]
+
+    [Tooltip("Object we want to collide with. Defaults to Player tag")]
+    [SerializeField] GameObject collidingObject;
+
+    [Tooltip("Check if this object should be deleted with colliding object")]
+    [SerializeField] bool deleteOnCollide = false;
+
+    [Tooltip("Check if this object is a pickup item and toggles trigger on collider component")]
+    [SerializeField] bool isPickup = false;
+
+    // Use this for initialization
+    void Start () {
+        // Get rigidbody of this component
+        //this.rigidbody = gameObject.GetComponent<Rigidbody>();
+
+        // Get instance of player
+        this.collidingObject = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // If we have checked isPickup, force collider to be a trigger
+        if (isPickup)
+        {
+            gameObject.GetComponent<Collider>().isTrigger = true;
+        }
+        else
+        {
+            gameObject.GetComponent<Collider>().isTrigger = false;
+        }
+    }
+
+    // Handle Trigger events
+    private void OnTriggerEnter(Collider other)
+    {
+        // Grab the root of each game object's transform so we don't compare children collisions
+        if (other.transform.root == this.collidingObject.transform.transform.root)
+        {
+            print(other.gameObject + " picked up " + this.gameObject + "!");
+        }
+        HandleDelete();
+    }
+
+    // Handle collision events
+    private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the collision is coming from the colliding object
+        if (collision.gameObject == this.collidingObject)
+        {
+            HandleDelete();
+        }
+    }
+
+    private void HandleDelete ()
+    {
+        if (this.deleteOnCollide)
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Rocket : MonoBehaviour {
+    // Hold rigid body of the rocket ship
+    Rigidbody rigidbody;
+    AudioSource audio;
+
+    [Header("Rocket Settings")]
+
+    [Tooltip("Speed of left or right rotation")]
+    [SerializeField] float rotationSpeed = 100f;
+    [Tooltip("Thrust speed of rocket")]
+    [SerializeField] float thrustSpeed = 100f;
+
+    // Use this for initialization
+    void Start () {
+        // Grab rigidbody component from gameobject
+        this.rigidbody = GetComponent<Rigidbody>();
+
+        // Grab audio source for rocket
+        this.audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Thrust();
+        Rotate();
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("okay");
+                break;
+            case "Pickup":
+                print("encountered pickup; might not ever occur because pickups are usually triggers");
+                break;
+            default: // Everything else makes us blow up
+                print("die");
+                break;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       // if (other.gameObject.CompareTag("Pick Up"))
+       // {
+        //    GameObject.Destroy(other.gameObject);
+       // }
+    }
+
+    private void Thrust()
+    {
+        // Check to see if spacebar is being pressed
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // Relative force will move the object in the direction specified relative to where it's currently positioned
+            float thrustThisFrame = thrustSpeed * Time.deltaTime;
+            rigidbody.AddRelativeForce(Vector3.up * thrustThisFrame);
+
+            // Play audio if it is not already playing
+            if (!audio.isPlaying)
+            {
+                audio.Play();
+            }
+        }
+        else
+        {
+            audio.Stop();
+        }
+    }
+
+    private void Rotate()
+    {
+        this.rigidbody.freezeRotation = true; // Take manual control of rotation; eliminates physics from 
+
+        float rotationThisFrame = rotationSpeed * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        this.rigidbody.freezeRotation = false;  // Resume physics control
+    }
+}
